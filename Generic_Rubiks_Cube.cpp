391eba231099fc5785cdc88c59f2a803464af2e5
AksPro-07 <aksprofficials1.1@gmail.com>
2025-06-17 10:47:13 +0530

    Initial commit - I initialised this project.

---
+++ Generic_Rubiks_Cube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by akshi on 17-06-2025.
+//
+
+#include "Generic_Rubiks_Cube.h"
---
+++ Generic_Rubiks_Cube.h
@@ -0,0 +1,16 @@
+//
+// Created by akshi on 17-06-2025.
+//
+
+#ifndef GENERIC_RUBIKS_CUBE_H
+#define GENERIC_RUBIKS_CUBE_H
+
+
+
+class Generic_Rubiks_Cube {
+
+};
+
+
+
+#endif //GENERIC_RUBIKS_CUBE_H
